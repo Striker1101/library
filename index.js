5a2e6df11adcc23dde19213e260028ed077c6e15
Striker <goodluckodemilin@gmail.com>
2022-09-21 19:53:02 -0700

    add button

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const form = document.querySelector('#books')
 let  myLibrary = []
 
 
-
 /*
 class book {
   constructor(title, author, pages, read) {
@@ -54,31 +53,36 @@ let lock = content.display()
     dpage.textContent = lock[2]
     dread.textContent = lock[3]*/
   let catalog = document.querySelector('#catalog');
+
+  let hold = document.createElement('div');
+  hold.classList.add('hold')
+  catalog.appendChild(hold)
+
   let dtitle = document.createElement("p");
   dtitle.classList.add('dtitle');
-  catalog.appendChild(dtitle);
+  hold.appendChild(dtitle);
   
   let dauthor = document.createElement("p");
   dauthor.classList.add('dauthor');
-  catalog.appendChild(dauthor);
+  hold.appendChild(dauthor);
   
   let dpage = document.createElement("p");
   dpage.classList.add('dpage');
-  catalog.appendChild(dpage);
+  hold.appendChild(dpage);
   
   let dread= document.createElement("p");
   dread.classList.add('dread');
-  catalog.appendChild(dread);
+  hold.appendChild(dread);
   
   let count = document.createElement("div");
   count.classList.add("count");
-  catalog.appendChild(count);
+  hold.appendChild(count);
 
     //toggle textContent of output
   let readToggle = document.createElement("button");
   readToggle.classList.add("readToggle");
   readToggle.setAttribute("type", "button")
-  catalog.appendChild(readToggle); 
+  hold.appendChild(readToggle); 
   readToggle.textContent = "Option"
  
 
@@ -86,7 +90,7 @@ let lock = content.display()
 let remove = document.createElement("button");
 remove.classList.add("remove");
 remove.setAttribute("type", "button")
-catalog.appendChild(remove); 
+hold.appendChild(remove); 
 remove.textContent = "Delete";
 
 readToggle.onclick = () => {if (dread.textContent == "unread"){
@@ -96,113 +100,32 @@ readToggle.onclick = () => {if (dread.textContent == "unread"){
  console.log(myLibrary)
 };
 //remove obj from arr
-remove.onclick = () => { myLibrary.splice([count.getAttribute("data-count")], 1);
-
-removeAllInnerText(catalog)
-myLibrary.forEach ((element, index)=> {
-  dtitle.textContent = element.title
-  dauthor.textContent = element.author
-  dpage.textContent = element.pages
-  dread.textContent = element.read
- // index = (myLibrary.indexOf(element))
- // set counter atttribte for obj
-  count.setAttribute("data-count", `${index}`);
-})
-console.log(myLibrary)
+remove.onclick = (e) => { 
+  myLibrary.splice([count.getAttribute("data-count")], 1);
+  e.target.parentElement.remove();
+  e.target.parentElement.style.display ="none"
 }
-
-
- 
+ function set(){
   myLibrary.forEach ((element, index)=> {
     dtitle.textContent = element.title
     dauthor.textContent = element.author
     dpage.textContent = element.pages
     dread.textContent = element.read
-   // index = (myLibrary.indexOf(element))
    // set counter atttribte for obj
     count.setAttribute("data-count", `${index}`);
-    //read or unread
   })
+ }
+ set()
 
 }
 
-const removeAllInnerText = (element) => {
-  const nodes = element.childNodes;
-
-  nodes.forEach((node) => {
-    if (node.nodeType === Node.ELEMENT_NODE && node.childNodes.length > 0) {
-      removeAllInnerText(node);
-    } else if (node.nodeType === Node.TEXT_NODE) {
-      node.textContent = "";
-    }
-  });
-};
 
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   form.reset();
 })
-/*
-myLibrary.forEach(item =>{
-  let lib = document.createElement("div").classList.add("lib")
-catalog.appendChild(lib);
 
-let dtitle = document.createElement("p").classList.add("dtitle")
-lib.appendChild(dtitle)
-
-let dauthor = document.createElement("p").classList.add("dauthor")
-dtitle.appendChild(dauthor)
-
-let dpage = document.createElement("p").classList.add("dpage")
-dauthor.appendChild(dpage)
-
-let dread = document.createElement("p").classList.add("dread")
-dpage.appendChild(dread)
-  
-})
 /*JSON.stringify(book);//turn each element to string
 let myLibrary = [(Object.values(book))]; // display all element in an object 
-
-function Book() {
- // the constructor...
- var book= function(author, title, pages, read){
-   this.author = author;
-   this.title  = title;
-   this.pages  = pages;
-   this.read   = read;
- }
-} 
-function removeBook() {
-  // do stuff here
- 
- }
-
-function addBookToLibrary() {
- // do stuff here
-var form= document.getElementById('books') .value; 
-document.getElementById('catalog').value= form;
-}
-
-const btn = document.getElementById('btn');
-
-btn.addEventListener('click', () => {
-  const form = document.getElementById('books');
-
-  if (form.style.display === 'none') {
-    // 👇️ this SHOWS the form
-    form.style.display = 'block';
-  } else {
-    // 👇️ this HIDES the form
-    form.style.display = 'none';
-  }
-});*/
-/*
-let button = document.querySelector("button");
-let first = document.querySelector("first");
-let second = document.createElement("div");
-second.classList.add("second");
-first.textContent = "first";
-second.textContent = "second"
-button.onclick = () => { second.classList.toggle("first")}
 */
